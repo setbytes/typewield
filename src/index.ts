06a56a0cc3eef60f2dcefb149f1de42ca28c17d3
Lucas Lopes <lucass.code@gmail.com>
2023-01-21 14:18:16 -0500

    chore: add .npmignore

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
 import 'module-alias/register';
-import { Logger } from '@/app/decorators/logger'
-class TestDecorator {
-  @Logger
-  logging(value: string, n: number) {
-    console.log('this is a test class', value, n)
-    return 'this is the return'
-  }
-}
-
-console.log('works', new TestDecorator().logging('HelloWorld', 3000))
+export * from '@/app/decorators/logger';
