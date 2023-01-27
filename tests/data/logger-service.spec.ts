1a85f7934f18fefb4e710c95e8b960fcceca810c
Lucas Lopes <lucass.code@gmail.com>
2023-01-27 18:50:04 -0500

    test: logger service

---
+++ tests/data/logger-service.spec.ts
@@ -0,0 +1,60 @@
+import { faker } from '@faker-js/faker'
+import { Logger } from '../../src/core/domain/logger-protocol'
+import { LoggerUseCase } from '../../src/core/usecase/logger-usecase'
+import { LoggerService } from '../../src/data/logger-service'
+
+describe('LoggerService', () => {
+  let logger: Logger
+  let loggerService: LoggerUseCase
+
+  beforeEach(() => {
+    logger = {
+      info: jest.fn(),
+      error: jest.fn(),
+      warn: jest.fn(),
+      log: jest.fn()
+    }
+    loggerService = new LoggerService(logger)
+  })
+
+  it('should call logger.info with input params and function name', () => {
+    const inputParams = [faker.random.word(), faker.random.word()]
+    const functionName = faker.random.word()
+    const originalFunction = jest.fn().mockReturnValue(inputParams[0])
+
+    loggerService.log(inputParams, functionName, originalFunction)
+
+    expect(logger.info).toHaveBeenCalledWith('[INPUT]', `[${functionName}]`, inputParams)
+  })
+
+  it('should call originalFunction with input params and return its result', () => {
+    const inputParams = [faker.random.word(), faker.random.word()]
+    const functionName = faker.random.word()
+    const originalFunction = jest.fn().mockReturnValue(inputParams[0])
+
+    const result = loggerService.log(inputParams, functionName, originalFunction)
+
+    expect(originalFunction).toHaveBeenCalledWith(...inputParams)
+    expect(result).toEqual(inputParams[0])
+  })
+
+  it('should call logger.info with output result and function name', async () => {
+    const inputParams = [faker.random.word(), faker.random.word()]
+    const functionName = faker.random.word()
+    const originalFunction = jest.fn().mockReturnValue(Promise.resolve(inputParams[0]))
+
+    await loggerService.log(inputParams, functionName, originalFunction)
+
+    expect(logger.info).toHaveBeenCalledWith('[INPUT]', `[${functionName}]`, inputParams)
+  })
+
+  it('should call logger.error with error message and function name', () => {
+    const inputParams = [faker.random.word(), faker.random.word()]
+    const functionName = faker.random.word()
+    const errorMessage = faker.random.word()
+    const originalFunction = jest.fn().mockImplementation(() => { throw new Error(errorMessage) })
+
+    expect(() => loggerService.log(inputParams, functionName, originalFunction)).toThrowError(errorMessage)
+    expect(logger.error).toHaveBeenCalledWith('[OUTPUT]', `[${functionName}]`, errorMessage)
+  })
+})
