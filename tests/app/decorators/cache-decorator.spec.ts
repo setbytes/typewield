2a4622593a1b6694b46fd889d5aa6d2c1d357831
Lucas Lopes <lucass.code@gmail.com>
2023-01-28 06:34:03 -0500

    test: ensure cache param decorator works

--- tests/app/decorators/cache-decorator.spec.ts
+++ tests/app/decorators/cache-decorator.spec.ts
@@ -1,36 +1,71 @@
 import { faker } from '@faker-js/faker'
-import { Cache } from '../../../src'
+import { Cache, CacheParam } from '../../../src'
 
-describe('@Cache Decorator', () => {
-  it('should run no async decorator successfuly', () => {
-    class CacheDecorator {
-      @Cache
-      run(value: string, num: number) {
-        return {value, num}
+describe('Cache Decorators', () => {
+  describe('@Cache', () => {
+    it('should run no async decorator successfuly', () => {
+      class CacheDecorator {
+        @Cache
+        run(value: string, num: number) {
+          return {value, num}
+        }
       }
-    }
-
-    const message = faker.random.word()
-    const num = Number(faker.random.numeric())
-    const resultNoCache = new CacheDecorator().run(message, num);
-    const resultWithCache = new CacheDecorator().run(message, num);
-    expect(resultNoCache).toStrictEqual({ value: message, num })
-    expect(resultWithCache).toStrictEqual({ value: message, num })
+  
+      const message = faker.random.word()
+      const num = Number(faker.random.numeric())
+      const resultNoCache = new CacheDecorator().run(message, num);
+      const resultWithCache = new CacheDecorator().run(message, num);
+      expect(resultNoCache).toStrictEqual({ value: message, num })
+      expect(resultWithCache).toStrictEqual({ value: message, num })
+    })
+  
+    it('should run async decorator successfuly', async () => {
+      class CacheDecorator {
+        @Cache
+        async run(value: string, num: number) {
+          return Promise.resolve({value, num})
+        }
+      }
+  
+      const message = faker.random.word()
+      const num = Number(faker.random.numeric())
+      const resultNoCache = await new CacheDecorator().run(message, num);
+      const resultWithCache = await new CacheDecorator().run(message, num);
+      expect(resultNoCache).toStrictEqual({ value: message, num })
+      expect(resultWithCache).toStrictEqual({ value: message, num })
+    })
   })
-
-  it('should run async decorator successfuly', async () => {
-    class CacheDecorator {
-      @Cache
-      async run(value: string, num: number) {
-        return Promise.resolve({value, num})
+  describe('@CacheParam', () => {
+    it('should run no async decorator successfuly', () => {
+      class CacheParamDecorator {
+        @CacheParam({ expire: 10 })
+        run(value: string, num: number) {
+          return {value, num}
+        }
       }
-    }
-
-    const message = faker.random.word()
-    const num = Number(faker.random.numeric())
-    const resultNoCache = await new CacheDecorator().run(message, num);
-    const resultWithCache = await new CacheDecorator().run(message, num);
-    expect(resultNoCache).toStrictEqual({ value: message, num })
-    expect(resultWithCache).toStrictEqual({ value: message, num })
+  
+      const message = faker.random.word()
+      const num = Number(faker.random.numeric())
+      const resultNoCache = new CacheParamDecorator().run(message, num);
+      const resultWithCache = new CacheParamDecorator().run(message, num);
+      expect(resultNoCache).toStrictEqual({ value: message, num })
+      expect(resultWithCache).toStrictEqual({ value: message, num })
+    })
+  
+    it('should run async decorator successfuly', async () => {
+      class CacheParamDecorator {
+        @CacheParam({ expire: 10 })
+        async run(value: string, num: number) {
+          return Promise.resolve({value, num})
+        }
+      }
+  
+      const message = faker.random.word()
+      const num = Number(faker.random.numeric())
+      const resultNoCache = await new CacheParamDecorator().run(message, num);
+      const resultWithCache = await new CacheParamDecorator().run(message, num);
+      expect(resultNoCache).toStrictEqual({ value: message, num })
+      expect(resultWithCache).toStrictEqual({ value: message, num })
+    })
   })
 })
