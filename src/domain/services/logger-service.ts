ee864a0fe0c798f70849eabbbab5bca4ddd3c62b
Lucas Lopes <lucass.code@gmail.com>
2025-05-23 08:31:07 -0400

    feat: add performace timer

--- src/domain/services/logger-service.ts
+++ src/domain/services/logger-service.ts
@@ -10,21 +10,25 @@ export class LoggerService implements LoggerUseCase {
 
   public log(params: Array<any>, functionName: string, originalFunction: Function, time?: number): any {
     const timer = time || Date.now();
-    if (params.length) {
-      this.logger.info("[INPUT]", `[${timer}]`, `[${functionName}]`, params);
-    }
+    const start = performance.now();
     try {
       const result = originalFunction(...params);
       if (result instanceof Promise) {
         return result.then((data) => {
-          this.logger.info("[OUTPUT]", `[${timer}]`, `[${functionName}]`, data);
+          const end = performance.now();
+          if (params.length) this.logger.info("[INPUT]", `[${timer}]`, `[${functionName}]`, params);
+          this.logger.info("[OUTPUT]", `[${timer}]`, `[${end - start}ms]`, `[${functionName}]`, data);
           return data;
         });
       }
-      this.logger.info("[OUTPUT]", `[${timer}]`, `[${functionName}]`, result);
+      const end = performance.now();
+      if (params.length) this.logger.info("[INPUT]", `[${timer}]`, `[${end - start}ms]`, `[${functionName}]`, params);
+      this.logger.info("[OUTPUT]", `[${timer}]`, `[${end - start}ms]`, `[${functionName}]`, result);
       return result;
     } catch (error) {
-      this.logger.error("[OUTPUT]", `[${timer}]`, `[${functionName}]`, error.message);
+      const end = performance.now();
+      if (params.length) this.logger.info("[INPUT]", `[${timer}]`, `[${end - start}ms]`, `[${functionName}]`, params);
+      this.logger.error("[OUTPUT]", `[${timer}]`, `[${end - start}ms]`, `[${functionName}]`, error.message);
       throw error;
     }
   }
