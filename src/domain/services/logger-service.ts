f056d4b8e562d5b29da6eca18b147b618bd97d63
Lucas Lopes <lucass.code@gmail.com>
2025-05-24 15:56:35 -0400

    fix: js bug

--- src/domain/services/logger-service.ts
+++ src/domain/services/logger-service.ts
@@ -21,6 +21,8 @@ export class LoggerService implements LoggerUseCase {
           this.logger.info("[OUTPUT]", `[${timer}]`, `[${milliseconds}ms]`, `[${functionName}]`, data);
           return data;
         }).catch(error => {
+          const end = performance.now();
+          const milliseconds = (end - start).toFixed(2);
           if (params.length) this.logger.info("[INPUT]", `[${timer}]`, `[${milliseconds}ms]`, `[${functionName}]`, params);
           this.logger.info("[OUTPUT]", `[${timer}]`, `[${milliseconds}ms]`, `[${functionName}]`, error);
           throw error;
